72974944a5ad21922f743a261e9c371f6fffe854
Zelderon7 <zelderon038@gmail.com>
2023-07-04 09:28:16 +0300

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.nio.charset.CharsetDecoder;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.Timer;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
