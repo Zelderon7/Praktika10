e1f633f85251e554460bbafbd34f333d3b53e6d5
Zelderon7 <zelderon038@gmail.com>
2023-07-05 11:26:27 +0300

    Homework day3/day4

---
+++ src/main/java/training_day3/HW/Zad1HW.java
@@ -0,0 +1,17 @@
+package training_day3.HW;
+
+public class Zad1HW {
+
+    public static void main(String[] args){
+
+    }
+
+    private static int Smallest(int a, int b, int c){
+        if(a < b)
+            return a < c? a:c;
+        else
+            return b < c? b:c;
+    }
+}
+
+
