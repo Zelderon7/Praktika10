fcd4dc1d8712db8edd06cb4918f26ba23d92d277
Zelderon7 <zelderon038@gmail.com>
2023-07-11 11:30:59 +0300

    Operation file

--- src/main/java/calculator/Operation.java
+++ src/main/java/calculator/Operation.java
@@ -1,4 +1,10 @@
 package calculator;
 
 public class Operation {
+    public char name;
+    public int priority;
+    public Operation(char name, int priority){
+        this.name = name;
+        this.priority = priority;
+    }
 }
