9eb625881572aaae1c8c21502700f9a35bbc3d9f
Zelderon7 <zelderon038@gmail.com>
2023-07-14 09:46:59 +0300

    Removing unnecessary constructor in Grade.java

--- src/main/java/training_day5/HW/Grade.java
+++ src/main/java/training_day5/HW/Grade.java
@@ -5,12 +5,6 @@ public class Grade{
     public int value;
     public Teacher teacher;
 
-    public Grade(int value, Teacher teacher){
-        this.value = value;
-        this.teacher = teacher;
-        teacher.signNewGrade(this);
-    }
-
     public Grade(int value, Teacher teacher, Student student, Subjects subject){
         this.value = value;
         this.teacher = teacher;
